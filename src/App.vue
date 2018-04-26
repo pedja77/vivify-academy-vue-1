41625c6dce82b1da33c7b19493ad803618c7d0d6
Nenad Vujicic <nndprogramer@gmail.com>
2018-04-26 21:04:02 +0200

    Commit 5

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,12 @@
 <template>
   <div id="app">
-    <ContactsList />
+    <button class="btn btn-primary">Hello Bootstrap</button>
   </div>
 </template>
 
 <script>
-import ContactsList from './components/ContactsList.vue'
-
 export default {
   name: 'app',
-  components: {
-    ContactsList
-  }
+  components: {}
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
